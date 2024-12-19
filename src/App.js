9eb613b443c8e3d072a355ca0addd4c689322da8
Coko_33 <nicolasplacido@gmail.com>
2024-12-19 07:43:11 -0300

    conexion con la bd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Turnos from "./Turnos";
 import "./App.css";
 import Calendario from "./Calendar";
+import Crud from "./Crud";
 
 function App() {
   return (
     <>
       <Turnos></Turnos>
       <Calendario></Calendario>
+      <Crud></Crud>
     </>
   );
 }
